d622da08aaf9dc6ab8b2cca223eecde55d77b2f2
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-06-05 16:44:05 -0400

    Adjust tests for this particular Ansible role

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize(
+    "f",
+    ["/root/.cobaltstrike.license", "/opt/cobaltstrike", "/opt/cobaltstrike/update"],
+)
+def test_files(host, f):
+    """Test that the expected files and directories are present."""
+    assert host.file(f).exists
+
+
+@pytest.mark.parametrize("f", ["/tmp/cobaltstrike.tgz"])
+def test_files2(host, f):
+    """Test that the expected files and directories were deleted."""
+    assert not host.file(f).exists
