6b08f472cf5a2318e538f944330461e677f5921c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-28 13:38:01 -0400

    Remove iam_usernames parameter
    We will just let the other account specify which users are allowed to
    assume this role.

--- terraform/iam_user.tf
+++ terraform/iam_user.tf
@@ -25,15 +25,7 @@ module "bucket_access" {
 
   account_ids = [data.aws_caller_identity.current.account_id]
   entity_name = aws_iam_user.user.name
-  # When the user is not yet created, Terraform does not correctly
-  # pick up the user's name from aws_iam_user.user.name when passing
-  # it in on the next line, so we fall back to using
-  # local.test_user_name.
-  #
-  # It is a mystery why aws_iam_user.user.name is acceptable on the
-  # lines that sandwich this one, but not this one.
-  iam_usernames = [local.test_user_name]
-  role_name     = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
+  role_name   = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
   role_tags = merge(var.tags,
     {
       "GitHub_Secret_Name"             = "TEST_ROLE_TO_ASSUME",
