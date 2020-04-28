3a4f53ba6312f0da886c84cdcf587e41bd0d22ae
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-28 13:30:50 -0400

    Attach an inline policy to the test user
    The policy allows the test user to assume the S3 read-only role and
    tag sessions.

--- terraform/iam_user.tf
+++ terraform/iam_user.tf
@@ -2,21 +2,21 @@ locals {
   test_user_name = "test-ansible-role-cobalt-strike"
 }
 
-# The user being created
+# Create the test user and its access key
 resource "aws_iam_user" "user" {
   provider = aws.users
 
   name = local.test_user_name
   tags = var.tags
 }
-
-# The IAM access key for the user
 resource "aws_iam_access_key" "key" {
   provider = aws.users
 
   user = aws_iam_user.user.name
 }
 
+# Create the role that allows read-only access to the particular S3
+# objects that are required by this Ansible role
 module "bucket_access" {
   source = "github.com/cisagov/s3-read-role-tf-module"
   providers = {
@@ -42,4 +42,29 @@ module "bucket_access" {
   )
   s3_bucket  = var.bucket_name
   s3_objects = var.objects
+}
+
+# Ensure that the test user is allowed to assume the bucket read-only
+# role
+data "aws_iam_policy_document" "assume_bucket_role" {
+  provider = aws.images
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    resources = [
+      module.bucket_access.role.arn,
+    ]
+  }
+}
+resource "aws_iam_user_policy" "assume_bucket_role" {
+  provider = aws.users
+
+  policy = data.aws_iam_policy_document.assume_bucket_role.json
+  user   = aws_iam_user.user.name
 }
