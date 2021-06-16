ed9f3e2359476c98d6722c503ab9c007739dd994
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-06-16 13:20:05 -0400

    Adapt code to work with recent changes to cisagov/s3-read-role-tf-module
    These changes were made in cisagov/s3-read-role-tf-module#15.

--- terraform/bucket_roles.tf
+++ terraform/bucket_roles.tf
@@ -7,16 +7,14 @@ module "production_bucket_access" {
   }
 
   account_ids = [data.aws_caller_identity.current.account_id]
+  additional_role_tags = {
+    "GitHub_Secret_Name"             = "TEST_ROLE_TO_ASSUME",
+    "GitHub_Secret_Terraform_Lookup" = "arn"
+  }
   entity_name = aws_iam_user.user.name
   role_name   = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
-  role_tags = merge(var.tags,
-    {
-      "GitHub_Secret_Name"             = "TEST_ROLE_TO_ASSUME",
-      "GitHub_Secret_Terraform_Lookup" = "arn"
-    }
-  )
-  s3_bucket  = var.production_bucket_name
-  s3_objects = var.production_objects
+  s3_bucket   = var.production_bucket_name
+  s3_objects  = var.production_objects
 }
 module "staging_bucket_access" {
   source = "github.com/cisagov/s3-read-role-tf-module"
@@ -27,7 +25,6 @@ module "staging_bucket_access" {
   account_ids = [data.aws_caller_identity.current.account_id]
   entity_name = aws_iam_user.user.name
   role_name   = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
-  role_tags   = var.tags
   s3_bucket   = var.staging_bucket_name
   s3_objects  = var.staging_objects
 }
