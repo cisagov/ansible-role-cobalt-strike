ff520f5c983ed5264a596f07f4b5aeec0a97cd85
David Redmin <david.redmin@trio.dhs.gov>
2021-09-22 10:51:46 -0400

    Bump required Terraform version to ~> 1.0

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,6 @@
 terraform {
-  # We want to hold off on 1.0 or higher until we have tested it.
-  required_version = "~> 0.14.0"
+  # We want to hold off on 1.1 or higher until we have tested it.
+  required_version = "~> 1.0"
 
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
