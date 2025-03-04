9e43d77adfd9db256c969abae87e60d6dc36329e
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-04 15:03:58 -0500

    Modify backend to use a partial configuration
    This lets us avoid hardcoding the bucket name and allows it to be set on a per-environment basis via the -backend-config command line option or other methods.  For details, see: https://developer.hashicorp.com/terraform/language/backend#partial-configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "cisa-cool-terraform-state"
+    # Use a partial configuration to avoid hardcoding the bucket name. This
+    # allows the bucket name to be set on a per-environment basis via the
+    # -backend-config command line option or other methods.  For details, see:
+    # https://developer.hashicorp.com/terraform/language/backend#partial-configuration
+    bucket         = ""
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
     key            = "skeleton-ansible-role-with-test-user/terraform.tfstate"
