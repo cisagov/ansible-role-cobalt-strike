edad170a25ca0d93c2246e48fd2f8062881aecec
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-05 11:34:17 -0500

    Remove env-specific resources related to the third-party bucket access role and policy
    Also, attach the bucket access policy to the role used by the Molecule test user.

--- terraform/bucket_roles.tf
+++ terraform/bucket_roles.tf
@@ -1,40 +1,22 @@
-# Create the roles that allow read-only access to the particular S3
+# Create the role that is allowed read-only access to the particular S3
 # objects that are required by this Ansible role
-#
-# Note that only the production role gets the additional role tags.
-# This is because the additional tags being applied mark the role as
-# being _the_ role to assume by this repo's AWS test user before
-# running molecule test. We create both a production and a staging
-# policy because any Packer repos that use this Ansible role will
-# attach the production or staging policy to the corresponding
-# production or staging Packer role; the production or staging Packer
-# role is assumed by the AWS Packer build user depending on whether a
-# production or staging AMI is being used, respectively.
-module "production_bucket_access" {
+module "bucket_access" {
   source = "github.com/cisagov/s3-read-role-tf-module"
   providers = {
-    aws = aws.images_production
+    aws = aws.images_provisionthirdpartybucketreadroles
   }
 
   account_ids = [data.aws_caller_identity.current.account_id]
-  additional_role_tags = {
-    "GitHub_Secret_Name"             = "TEST_ROLE_TO_ASSUME",
-    "GitHub_Secret_Terraform_Lookup" = "arn"
-  }
-  entity_name = aws_iam_user.user.name
-  role_name   = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
-  s3_bucket   = var.production_bucket_name
-  s3_objects  = var.production_objects
+  entity_name = module.user.user.name
+  role_name   = "ThirdPartyBucketRead-${module.user.user.name}"
+  s3_bucket   = var.cobalt_strike_bucket
+  s3_objects  = var.cobalt_strike_objects
 }
-module "staging_bucket_access" {
-  source = "github.com/cisagov/s3-read-role-tf-module"
-  providers = {
-    aws = aws.images_staging
-  }
 
-  account_ids = [data.aws_caller_identity.current.account_id]
-  entity_name = aws_iam_user.user.name
-  role_name   = "ThirdPartyBucketRead-${aws_iam_user.user.name}"
-  s3_bucket   = var.staging_bucket_name
-  s3_objects  = var.staging_objects
+# Attach the bucket access policy to the role used by the test user
+resource "aws_iam_role_policy_attachment" "bucket_access" {
+  provider = aws.images_provisionaccount
+
+  policy_arn = module.bucket_access.policy.arn
+  role       = module.user.role.name
 }
