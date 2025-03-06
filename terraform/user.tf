64bdfb4f0d3b352170b629c2428abd1dfabea6ee
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-06 11:10:29 -0500

    Replace iam_user.tf with our standard user.tf

--- terraform/user.tf
+++ terraform/user.tf
@@ -8,6 +8,8 @@ module "user" {
     aws.images-ssm              = aws.images_ssm
   }
 
-  entity         = "skeleton-ansible-role-with-test-user"
-  ssm_parameters = ["/example/parameter"]
+  entity = "ansible-role-cobalt-strike"
+
+  # No SSM access is needed for this user, but ssm_parameters cannot be empty
+  ssm_parameters = ["/not/used"]
 }
