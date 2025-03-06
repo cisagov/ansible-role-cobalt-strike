229141eeb0d5e73c3da9f43fe6717b38c0bb439d
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-06 11:20:01 -0500

    Only output a single bucket access role and policy
    Remove references to staging and production.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,11 @@
-output "production_policy" {
-  value       = module.production_bucket_access.policy
-  description = "The IAM policy that can read the specified objects from the specified S3 production bucket."
+output "bucket_access_policy" {
+  value       = module.bucket_access.policy
+  description = "The IAM policy that can read the specified objects from the specified S3 bucket."
 }
 
-output "staging_policy" {
-  value       = module.staging_bucket_access.policy
-  description = "The IAM policy that can read the specified objects from the specified S3 staging bucket."
-}
-
-output "role" {
-  value       = module.user.role
-  description = "The IAM role that the CI user can assume to read SSM parameters in the Images account."
+output "bucket_access_role" {
+  value       = module.bucket_access.role
+  description = "The IAM role that can read the specified objects from the specified S3 bucket."
 }
 
 output "user" {
