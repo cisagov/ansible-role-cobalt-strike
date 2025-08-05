43aa915948f8336e3e5b1cc5db79a735a119e541
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-08-05 15:08:18 -0400

    Require at least version 1.1 of Terraform
    Version 1.1 of Terraform is the first version to support the nullable
    key in variable definitions, and we are making use of nullable in this
    Terraform code.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,7 @@
 terraform {
-  # We want to hold off on 1.1 or higher until we have tested it.
-  required_version = "~> 1.0"
+  # Version 1.1 of Terraform is the first version to support the
+  # nullable key in variable definitions.
+  required_version = "~> 1.1"
 
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
