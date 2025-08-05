b11f503d0fd6c6eed6e51789c4cc0068867c5cbd
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-08-05 15:17:29 -0400

    Use the latest AWS provider for Terraform
    I verified that this version works for this repository by re-running
    terraform apply for our dev-a, staging-a, and production deployments.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,18 +7,11 @@ terraform {
   # major version currently being used.  This practice will help us
   # avoid unwelcome surprises.
   required_providers {
-    # Version 4.9 of the Terraform AWS provider made changes to the S3 bucket
-    # refactor that is in place for versions 4.0-4.8 of the provider. With v4.9
-    # only non-breaking changes and deprecation notices are introduced. Using
-    # this version will simplify migration to the new, broken out AWS S3 bucket
-    # configuration resources. Please see
-    # https://github.com/hashicorp/terraform-provider-aws/pull/23985
-    # for more information about the changes in v4.9 and
-    # https://www.hashicorp.com/blog/terraform-aws-provider-4-0-refactors-s3-bucket-resource
-    # for more information about the S3 bucket refactor.
+    # We have verified that our code works with version 6.7 of this
+    # Terraform provider.
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.9"
+      version = "~> 6.7"
     }
   }
 }
