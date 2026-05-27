1ff96d525617b1f0710729ecca3d25a344653198
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-27 16:26:24 -0400

    Fix an overly long line
    This gets rid of a B950 error from our flake8 pre-commit linter.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -63,9 +63,10 @@ def test_version_and_license(host):
             assert (
                 False
             ), f"Unable to parse {version} or {version}.0 as a valid semantic version."
-    assert (
-        actual_version >= min_expected_version
-    ), f"Cobalt Strike version is expected to be greater than or equal to {min_expected_version}."
+    assert actual_version >= min_expected_version, (
+        "Cobalt Strike version is expected to be greater than or equal to "
+        f"{min_expected_version}."
+    )
 
     licensed = match.group("licensed")
     assert licensed == expected_licensed_value, "Cobalt Strike is not licensed."
